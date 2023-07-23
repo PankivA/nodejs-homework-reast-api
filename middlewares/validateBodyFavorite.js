d7adcb85cb30bc7301dbe8e05582ef11fff55a88
Andrii Pankiv <pankivand@gmail.com>
2023-07-23 20:19:22 +0300

    add db and hw3

---
+++ middlewares/validateBodyFavorite.js
@@ -0,0 +1,17 @@
+const { HttpError } = require("../helpers");
+
+const validateBodyFavorite = (schema) => {
+  const func = (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      res.status(400).json({
+        message: `missing field ${error.details[0].context.key} `,
+      });
+      return;
+    }
+    next();
+  };
+  return func;
+};
+
+module.exports = validateBodyFavorite;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,23 +1,22 @@
 const express = require('express')
 
-const { getAll, getById, addItem, deleteById, updateById } = require('../../controllers/controllers');
+const { getAll, getById, addItem, deleteById, updateById, updateStatusContact } = require('../../controllers/controllers');
+const { validateBody, validateBodyFavorite, isValidId } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
 
 const router = express.Router()
 
-const { validateBody } = require("../../middlewares");
-
-const schemas = require("../../schemas/contacts");
 
 router.get("/", getAll);
 
-router.get("/:contactId", getById);
+router.get("/:contactId", isValidId, getById);
 
 router.post("/", validateBody(schemas.addSchema), addItem);
 
-router.delete("/:contactId", deleteById);
+router.delete("/:contactId", isValidId, deleteById);
 
-router.put("/:contactId", validateBody(schemas.addSchema), updateById);
+router.put("/:contactId", isValidId, validateBody(schemas.addSchema), updateById);
 
-module.exports = router;
+router.patch("/:contactId/favorite", isValidId, validateBodyFavorite(schemas.updateFavoriteSchema), updateStatusContact);
 
 module.exports = router
