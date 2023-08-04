cf90daad5b7f826c649a60b5ebf147009102bcd4
Andrii Pankiv <pankivand@gmail.com>
2023-08-04 17:12:00 +0300

    add midleware auth

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,22 +1,22 @@
 const express = require('express')
 
 const { getAll, getById, addItem, deleteById, updateById, updateStatusContact } = require('../../controllers/controllers');
-const { validateBody, validateBodyFavorite, isValidId } = require("../../middlewares");
+const { validateBody, validateBodyFavorite, isValidId, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
 
 const router = express.Router()
 
 
-router.get("/", getAll);
+router.get("/", authenticate, getAll);
 
-router.get("/:contactId", isValidId, getById);
+router.get("/:contactId", authenticate, isValidId, getById);
 
-router.post("/", validateBody(schemas.addSchema), addItem);
+router.post("/", authenticate, validateBody(schemas.addSchema), addItem);
 
-router.delete("/:contactId", isValidId, deleteById);
+router.delete("/:contactId", authenticate, isValidId, deleteById);
 
-router.put("/:contactId", isValidId, validateBody(schemas.addSchema), updateById);
+router.put("/:contactId", authenticate, isValidId, validateBody(schemas.addSchema), updateById);
 
-router.patch("/:contactId/favorite", isValidId, validateBodyFavorite(schemas.updateFavoriteSchema), updateStatusContact);
+router.patch("/:contactId/favorite", authenticate, isValidId, validateBodyFavorite(schemas.updateFavoriteSchema), updateStatusContact);
 
 module.exports = router
